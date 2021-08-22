c807c536a04f85d100c643d0ee0aff198bc1677d
Ievgen-Rybalko <jackrjack1@gmail.com>
2021-08-22 23:54:17 +0300

    upd: classes replaced with hooks

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -1,9 +1,86 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import styles from './ContactForm.module.css';
 
-class ContactForm extends Component {
+function ContactForm({ onNewContactAdd }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = event => {
+    const { name, value } = event.currentTarget;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        console.warn(`Type of input cannot be handled!`);
+    }
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    //onFormSubmit(this.state);
+    onNewContactAdd(name, number);
+    reset();
+  };
+
+  const reset = () => {
+    setNumber('');
+    setName('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className={styles.title}>
+          Name
+          <input
+            className={styles.button}
+            type="text"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+          />
+        </label>
+
+        <label className={styles.title}>
+          Number
+          <input
+            className={styles.button}
+            type="tel"
+            name="number"
+            value={number}
+            onChange={handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять из цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+          />
+        </label>
+
+        <button type="submit" className={styles.button + ' ' + styles.small}>
+          Сохранить
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ContactForm;
+
+ContactForm.propTypes = {
+  onNewContactAdd: PropTypes.func,
+};
+
+/*class ContactForm extends Component {
   static propTypes = {
     name: PropTypes.string,
     number: PropTypes.string,
@@ -71,6 +148,4 @@ class ContactForm extends Component {
       </div>
     );
   }
-}
-
-export default ContactForm;
+}*/
